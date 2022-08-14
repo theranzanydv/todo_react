af9f9cc768678b16666ee08b7afef79acba18040
tilasmi13 <86902134+tilasmi13@users.noreply.github.com>
2022-08-14 22:19:40 +0545

    to-do list

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
 
-import Navbar from "./Components/Navbar/Navbar";
-import Header from "./Container/Header/Header";
+import './App.css';
 
-const App = () => {
+import Navbar from './components/Navbar';
+import FormInput from './components/FormInput';
+import List from './components/List';
+import FormFooter from './components/FormFooter';
+import { DataProvider } from './contexts/DataProvider';
+
+function App() {
 	return (
-		<div className="App">
-			<Navbar />
-			<Header />
-		</div>
+		<DataProvider>
+			<div className='App'>
+				<Navbar />
+				<div className='Todo'>
+					<h1>To Do List</h1>
+					<FormInput />
+					<List />
+					<FormFooter />
+				</div>
+			</div>
+		</DataProvider>
 	);
-};
+}
 
 export default App;
